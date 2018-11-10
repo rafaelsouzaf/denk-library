e0438f7d8df98c2000a9a03ce8e0daa37288106d
Rafael S. Fijalkowski <rafaelsouzaf@gmail.com>
2018-11-10 20:01:37 +0100

    Adding Book controller logic

--- src/main/java/github/com/rafaelsouzaf/library/controller/BookController.java
+++ src/main/java/github/com/rafaelsouzaf/library/controller/BookController.java
@@ -1,14 +1,53 @@
 package github.com.rafaelsouzaf.library.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import github.com.rafaelsouzaf.library.exception.BookNotFoundException;
+import github.com.rafaelsouzaf.library.model.Book;
+import github.com.rafaelsouzaf.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path="/book")
 public class BookController {
 
-    @RequestMapping("/book/list")
-    public String index() {
-        return "Listing books!";
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/list")
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+
+    }
+
+    @GetMapping("/get/{id}")
+    public Book get(@PathVariable Long id) throws BookNotFoundException {
+        return bookRepository
+                .findById(id)
+                .orElseThrow(() -> new BookNotFoundException(id));
+    }
+
+    @PutMapping("/add")
+    public Book add(@RequestBody Book user) {
+        return bookRepository.save(user);
+    }
+
+    @PutMapping("/edit/{id}")
+    public Book edit(@RequestBody Book newBook, @PathVariable Long id ) throws BookNotFoundException {
+        return bookRepository.findById(id)
+                .map(book -> {
+                    book.setTitle(newBook.getTitle());
+                    book.setBookAuthor(newBook.getBookAuthor());
+                    return bookRepository.save(book);
+                })
+                .orElseThrow(() -> new BookNotFoundException(id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id) throws BookNotFoundException {
+        if (!bookRepository.existsById(id)) {
+            throw new BookNotFoundException(id);
+        }
+        bookRepository.deleteById(id);
     }
 
 }
