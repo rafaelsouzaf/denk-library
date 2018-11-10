8805f27ffb40fe76a1356118b14162380666af6a
Rafael S. Fijalkowski <rafaelsouzaf@gmail.com>
2018-11-10 16:03:26 +0100

    Adding custom Exception

---
+++ src/main/java/github/com/rafaelsouzaf/library/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package github.com.rafaelsouzaf.library.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(Long id) {
+        super("User not found: " + id);
+    }
+}
