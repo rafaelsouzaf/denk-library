f26718b759ee9b35f7763dceb8ec7a4a135c8fba
Rafael S. Fijalkowski <rafaelsouzaf@gmail.com>
2018-11-11 13:10:41 +0100

    Adding more security rules

--- src/main/java/github/com/rafaelsouzaf/library/controller/LoginController.java
+++ src/main/java/github/com/rafaelsouzaf/library/controller/LoginController.java
@@ -2,10 +2,16 @@ package github.com.rafaelsouzaf.library.controller;
 
 import github.com.rafaelsouzaf.library.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping(path="/login")
@@ -19,9 +25,12 @@ public class LoginController {
         return "Login!";
     }
 
-    @PostMapping("/logout")
-    public String logout() {
-        return "Logout!";
+    @GetMapping("/logout")
+    public void logout (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
     }
 
 }
--- src/main/java/github/com/rafaelsouzaf/library/security/SecurityConfiguration.java
+++ src/main/java/github/com/rafaelsouzaf/library/security/SecurityConfiguration.java
@@ -1,17 +1,10 @@
 package github.com.rafaelsouzaf.library.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(
@@ -21,46 +14,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 )
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private static String REALM="MY_TEST_REALM";
-
 //    @Bean
 //    public BCryptPasswordEncoder encoder() {
 //        return new BCryptPasswordEncoder();
 //    }
 
-    @Autowired
-    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("bill").password("{noop}password").roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("tom").password("{noop}password").roles("USER");
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .anyRequest().fullyAuthenticated()
-//                .and()
-//                .formLogin(); // You probably need more than this
-
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
-//                .antMatchers("/user/**").hasRole("ADMIN")
-
-                .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-
+        http.httpBasic();
     }
 
-    @Bean
-    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
-        return new CustomBasicAuthenticationEntryPoint();
-    }
-
-    /* To allow Pre-flight [OPTIONS] request from browser */
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER").and()
+                .withUser("admin").password("{noop}password").roles("ADMIN");
     }
+
 }
