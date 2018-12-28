a45f8b6e0884bf5bacacb5c316cd5e57998ab5aa
rsouza <rafael.souza.fijalkowski@denkwerk.com>
2018-12-28 17:34:49 +0100

    jdbcAuthentication

--- src/main/java/github/com/rafaelsouzaf/library/security/SecurityConfiguration.java
+++ src/main/java/github/com/rafaelsouzaf/library/security/SecurityConfiguration.java
@@ -1,10 +1,16 @@
 package github.com.rafaelsouzaf.library.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableGlobalMethodSecurity(
@@ -14,17 +20,43 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 )
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and().csrf().disable();
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}password").roles("ADMIN");
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        System.out.println("================================ " + passwordEncoder().encode("password")); // $2a$10$XOhN2CvVncf0FThogN4QA.vJj3hfdiYaXlFLlRKJqYL1JhDPBVpYa
+
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT email as username, password FROM lib_user where email=?")
+//                .authoritiesByUsernameQuery("SELECT users.email as username,user_role.role_code as user_role FROM users inner join user_role on users.user_id=user_role.user_id where users.email=?")
+                .authoritiesByUsernameQuery("SELECT email as username, 'ADMIN' as user_role FROM lib_user where email=?");
+//                .and()
+//                .inMemoryAuthentication()
+//                .passwordEncoder(null)
+//                .withUser("user").password(passwordEncoder().encode("password")).roles("USER").and()
+//                .withUser("admin").password(passwordEncoder().encode("password")).roles("ADMIN");
+    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("{noop}password").roles("USER")
+//                .and()
+//                .withUser("admin").password("{noop}password").roles("ADMIN");
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
